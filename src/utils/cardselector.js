57579d5120c174f1806b00f4e9ae7d05cb0d00e2
Rajat Sharma <lunasunkaiser@gmail.com>
2017-07-27 12:53:51 +0530

    Added Posters

--- src/utils/cardselector.js
+++ src/utils/cardselector.js
@@ -7,11 +7,11 @@
 // import shield from '../statics/xzclow-shield.jpg'
 // import jump from '../statics/xzclow-jump.jpg'
 
-const earthy = 'https://vignette4.wikia.nocookie.net/marveldatabase/images/f/f1/Ant-Man_and_the_Wasp_poster_001.jpg/revision/latest/scale-to-width-down/311?cb=20170723162625'
+const earthy = 'https://vignette4.wikia.nocookie.net/marveldatabase/images/9/9c/Thor_Theatrical_Poster.jpg/revision/latest/scale-to-width-down/324?cb=20120605120807'
 const dark = 'https://vignette3.wikia.nocookie.net/marveldatabase/images/3/30/Thor_Ragnarok_poster_002.jpg/revision/latest/scale-to-width-down/324?cb=20170723014825'
 const wave = 'https://vignette1.wikia.nocookie.net/marveldatabase/images/d/d6/Ant-Man_%28film%29_poster_001.jpeg/revision/latest/scale-to-width-down/324?cb=20150506151531'
 const shot = 'https://vignette2.wikia.nocookie.net/marveldatabase/images/2/20/Ironmanposter.jpg/revision/latest/scale-to-width-down/324?cb=20080502172105'
-const sword = 'https://vignette4.wikia.nocookie.net/marveldatabase/images/3/31/Captain_America_The_Winter_Soldier_Teaser_Poster.jpg/revision/latest/scale-to-width-down/324?cb=20130709163720'
+const sword = 'https://vignette4.wikia.nocookie.net/marveldatabase/images/3/37/Captain_America_The_First_Avenger_poster.jpg/revision/latest?cb=20110725134313'
 const theReturn = 'https://vignette4.wikia.nocookie.net/marveldatabase/images/e/ee/Avengers_Age_of_Ultron_poster_001.jpg/revision/latest/scale-to-width-down/324?cb=20150224171724'
 const shield = 'https://vignette2.wikia.nocookie.net/marveldatabase/images/f/f7/Spider-Man_Homecoming_poster_010.jpg/revision/latest/scale-to-width-down/324?cb=20170626205049'
 const jump = 'https://vignette3.wikia.nocookie.net/marveldatabase/images/b/b2/Doctor_Strange_%28film%29_poster_004.jpg/revision/latest/scale-to-width-down/335?cb=20160908171707'
